c360b2e52ddfa73b432af6cb527d96ca471c91bd
mentai-mayo <102917864+mentai-mayo@users.noreply.github.com>
2022-04-20 16:02:12 +0900

    足し算と引き算を追加

--- 9cc.c
+++ 9cc.c
@@ -9,11 +9,38 @@ int main(int argc, char *argv[]){
     return 1;
   }
 
+  char *p = argv[1];
+
   // コンパイラメイン部分
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+
+  // まずは最初の数をセットする
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  // 残り
+  while(*p){
+
+    // +が来たとき
+    if(*p == '+'){
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    // -が来たとき
+    if(*p == '-'){
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    // 他
+    fprintf(stderr, "[9cc] unexpected character: '%c'(%d)\n", *p, *p);
+    return 1;
+  }
+
   printf("  ret\n");
 
   return 0;
